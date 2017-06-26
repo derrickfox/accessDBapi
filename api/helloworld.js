e0d52521e0c8d8f58d9317b95fb1036d679463b5
derrickfox <fox.derrick@gmail.com>
2017-06-25 22:16:41 -0400

    Updates

--- api/helloworld.js
+++ api/helloworld.js
@@ -5,7 +5,12 @@ function getRequest(request, response, next) {
 }
 
 helloService.routes = [
-    { path: '/hello', httpMethod: 'GET', middleware: getRequest }
+    { path: '/hello', httpMethod: 'GET', middleware: getRequest },
+    { path: '/new', httpMethod: 'POST', middleware: createNewUser },
+    { path: '/read/:id', httpMethod: 'GET', middleware: getSpecificUser },
+    { path: '/update/:id', httpMethod: 'POST', middleware: updateUser },
+    { path: '/delete/:id', httpMethod: 'POST', middleware: deleteUser },
+    { path: '/getAll', httpMethod: 'GET', middleware: getAllUsers }
 ]
 
 
@@ -26,8 +31,8 @@ connection
 
 function getAllUsers(request, response, next) {
     connection
-        .query('SELECT *' +
-            'FROM [customerList]')
+        .query( 'SELECT *' +
+                'FROM [customerList]')
         .on('done', function (data) {
             response.send(data);
         });
@@ -44,5 +49,29 @@ function getSpecificUser(request, response, next, id) {
 }
 
 function createNewUser(request, response, next, firstName, lastName, email, state) {
+    connection
+        .execute('INSERT INTO customerList(firstName, lastName, email, state) VALUES ("John", "Smith", "email@email.com", "MD")')
+        .on('done', function (data){
+            response.send(data);
+        });
+}
 
+//FIX THIS
+function updateUser(request, response, next, firstName, lastName, email, state){
+    connection
+        .execute('UPDATE customerList(firstName, lastName, email, state) VALUES ("John", "Smith", "email@email.com", "MD")')
+        .on('done', function (data){
+            response.send(data);
+        });
+}
+
+//FIX THIS
+function deleteUser(request, response, next, id) {
+    connection
+        .query( 'SELECT *' +
+            'FROM [customerList]' +
+            'WHERE id=' + id + ';')
+        .on('done', function (data){
+            response.send(data);
+        });
 }
