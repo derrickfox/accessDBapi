a54b28f4eab390bf5d7238d4358b1c67260164de
Derrick Fox <fox.derrick@gmail.com>
2017-07-02 22:10:44 -0400

    Delete function working

--- api/contactsAPI.js
+++ api/contactsAPI.js
@@ -9,11 +9,33 @@ function getAll(request, response, next) {
             response.send(data);
         })
         .catch(function (err) {
-            response.status(500).send({error: 'ERROR from failure() function: ' + err});
+            response.status(500).send({error: 'ERROR from showAll() function: ' + err});
         });
 
 };
 
+function createNew(request, response, next) {
+    contactsLogic.createNew()
+        .then(function (data) {
+            request.send(data);
+        })
+        .catch(function (err) {
+            response.status(500).send({error: 'ERROR from createNew() function: ' + err})
+        })
+}
+
+function deleteContact(request, response, next) {
+    contactsLogic.deleteContact({_id: request.params.id})
+        .then(function () {
+            request.send({_id: request.params.id});
+        })
+        .catch(function (err) {
+            response.status(500).send({error: 'ERROR from deleteContact() function: ' + err})
+        })
+}
+
 apiService.routes = [
-    { path: '/getAll', httpMethod: 'GET', middleware: getAll }
+    { path: '/getAll', httpMethod: 'GET', middleware: getAll },
+    { path: '/new', httpMethod: 'POST', middleware: createNew },
+    { path: '/delete/:id', httpMethod: 'DELETE', middleware: deleteContact }
 ]
