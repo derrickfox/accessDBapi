81b1f28aed18047924fb14bec1e70a2a2e54a503
derrickfox <fox.derrick@gmail.com>
2017-07-03 09:20:49 -0400

    Essentially done. Just need to handle response errors.

--- api/contactsAPI.js
+++ api/contactsAPI.js
@@ -34,8 +34,31 @@ function deleteContact(request, response, next) {
         })
 }
 
+function updateContact(request, response, next) {
+    contactsLogic.updateContact({_id: request.params.id})
+        .then(function () {
+            request.send({_id: request.params.id});
+        })
+        .catch(function (err) {
+            response.status(500).send({error: 'ERROR from updateContact() function:' + err})
+        })
+}
+
+function getOneContact(request, response, next) {
+    contactsLogic.getOne({_id: request.params.id})
+        .then(function (data) {
+            //request.send({_id: request.params.id});
+            response.send(data)
+        })
+        .catch(function (err) {
+            response.status(500).send({error: 'ERROR from getOneContact() function: ' + err});
+        });
+}
+
 apiService.routes = [
     { path: '/getAll', httpMethod: 'GET', middleware: getAll },
     { path: '/new', httpMethod: 'POST', middleware: createNew },
-    { path: '/delete/:id', httpMethod: 'DELETE', middleware: deleteContact }
+    { path: '/delete/:id', httpMethod: 'DELETE', middleware: deleteContact },
+    { path: '/update/:id', httpMethod: 'PATCH', middleware: updateContact},
+    { path: '/getOne/:id', httpMethod: 'GET', middleware: getOneContact}
 ]
